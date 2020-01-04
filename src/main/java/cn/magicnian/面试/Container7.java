567940cd6dc5583194a31620d9f3e7d7b07632b5
94496 <moshunian@163.com>
2020-01-04 14:08:06 +0800

    add one

--- src/main/java/cn/magicnian/面试/Container7.java
+++ src/main/java/cn/magicnian/面试/Container7.java
@@ -9,26 +9,29 @@ import java.util.List;
  * 线程2实现监控元素的个数
  * 当个数到5个时，线程2给出提示，并结束线程2
  */
-public class Container1 {
+public class Container7 {
+
+    private volatile int state;
 
     private List<String> list = new ArrayList<>();
 
-    public void add(String s){
+    public void add(String s) {
         list.add(s);
     }
 
-    public int size(){
+    public int size() {
         return list.size();
     }
 
+
     public static void main(String[] args) {
 
-        Container1 container = new Container1();
+        Container7 container = new Container7();
 
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i=0;i<10;i++){
+                for (int i = 0; i < 10; i++) {
                     container.add("value");
                     System.out.println("添加完成");
                 }
@@ -39,8 +42,8 @@ public class Container1 {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(true){
-                    if(container.size()==5){
+                while (true) {
+                    if (container.size() == 5) {
                         System.out.println("====元素个数已经达到5");
                         throw new RuntimeException("exit");
                     }
